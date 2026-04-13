bed855233b4911188805e32b50e0a99729276c9f
tuerre <ronnyduran35@gmail.com>
2026-04-13 10:42:35 -0400

    fix: restore APP_TYPE and APP_TENANT_ID for SingleTenant auth

--- config.py
+++ config.py
@@ -8,6 +8,8 @@ class DefaultConfig:
     PORT = int(os.environ.get("PORT", 8000))
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_TYPE = os.environ.get("MicrosoftAppType", "SingleTenant")
+    APP_TENANT_ID = os.environ.get("MicrosoftAppTenantId", "")
 
     # Azure Translator
     TRANSLATOR_KEY = os.environ.get("TRANSLATOR_KEY", "")
